19b77158ab3be2e0e47a7b40c67fbf571abd88dc
Amine-Arrachid <135894161+Amine-Arrachid@users.noreply.github.com>
2025-04-07 01:23:41 -0400

    Add files via upload

---
+++ src/test/A.operationTest2.java
@@ -0,0 +1,36 @@
+
+package soen.tutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class ArithmeticOperationsTest {
+
+
+	@Test
+	public void testAdd()
+	{
+
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.addOrSub(2, 6);
+		Integer expected = 8;
+		//assertEquals(expected, actual);
+		fail("must fail");
+
+	}
+	
+
+    /*@Test
+    public void testSub()
+    {
+
+        ArithmeticOperations operations = new ArithmeticOperations();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        assertEquals(expected, actual);
+
+    }*/
+
+}
